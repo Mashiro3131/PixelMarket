e44006211af3edd9e19f59b3c3d553c7a6bf3c6a
Nico Mengisen <mengisennico@gmail.com>
2026-05-18 14:00:22 +0200

    fix: standardisation d'acceès aux variables d'env pour la config SMTP

--- config.py
+++ config.py
@@ -16,12 +16,12 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Config du serveur SMTP
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
-    MAIL_USE_TLS = True # Sécurité de cryptage mail
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER', 'noreply@pixelmarket.com')
+    MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
+    MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
 
     # Config d'upload de fichiers
     UPLOAD_FOLDER = os.path.join(basedir, 'static', 'uploads')
@@ -38,3 +38,4 @@ class Config:
 # Flask-Mail config :   https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-x-email-support
 # os.path :             https://docs.python.org/3/library/os.path.html
 # Flask-Debug :         https://flask.palletsprojects.com/en/stable/config/
+# MDP App Gmail :       https://support.google.com/accounts/answer/185833?dark=1&hl=en-GB&utm_source=google-account&utm_medium=search-screen
